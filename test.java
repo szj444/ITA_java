bd6c4e57ff96fde011edcaa5d9b1510b15757b4e
lixinran1006 <q18782971845@126.com>
2025-04-21 22:34:33 +0800

    Delete test.java

--- test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    public static void main(String []args){
-        xxs1 xll=new xxs1();
-        xll.attack();
-        System.out.println(xll.speed());
-        xll.greet();
-        xll.move();
-    }
-}
